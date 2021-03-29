0cc090ac59912ae7b1bf9cab9149aa4d32508445
s585 <andrey.svetov@gmail.com>
2021-03-29 16:21:31 +0300

    initial commit

---
+++ src/main/java/com/andersenlab/usertrackerspring/UserTrackerSpringApplication.java
@@ -0,0 +1,13 @@
+package com.andersenlab.usertrackerspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTrackerSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserTrackerSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andersenlab/usertrackerspring/UserTrackerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersenlab.usertrackerspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserTrackerSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
